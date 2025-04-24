be0d4579c545452ccafe960a8095061873ff4d47
aliparsa <aliparsa9@gmail.com>
2025-04-24 17:10:23 +0430

    Set frontend index page

---
+++ resources/views/Frontend/layouts/main.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--=== Favicon ===-->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+    <title>Cardoor - Car Rental HTML Template</title>
+</head>
+
+<body class="loader-active">
+
+    <!--== Preloader Area Start ==-->
+    <div class="preloader">
+        <div class="preloader-spinner">
+            <div class="loader-content">
+                <img src="assets/img/preloader.gif" alt="JSOFT">
+            </div>
+        </div>
+    </div>
+    <!--== Preloader Area End ==-->
+
+    <!--== Header Area Start ==-->
+    @include('Frontend.layouts.header')
+    @yield('content')
+    <!--== Footer Area Start ==-->
+    @include('Frontend.layouts.footer')
+    <!--== Footer Area End ==-->
+
+    <!--== Scroll Top Area Start ==-->
+    <div class="scroll-top">
+        <img src="assets/img/scroll-top.png" alt="JSOFT">
+    </div>
+    <!--== Scroll Top Area End ==-->
+
+</body>
+
+</html>
